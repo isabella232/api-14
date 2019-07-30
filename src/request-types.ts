4c47ec1f3e5bf8e51b4315db28df668f9630a60f
moody <moody.salem@gmail.com>
2019-07-30 08:56:56 -0400

    change the alias

--- src/request-types.ts
+++ src/request-types.ts
@@ -19,7 +19,7 @@ export interface CreateAccountParams {
 export const NameValidator = jointz.string().minLength(1).maxLength(100);
 export const DescriptionValidator = jointz.string().minLength(1).maxLength(1000);
 export const VersionValidator = jointz.number().integer().min(1);
-export const EnsNameValidator = jointz.string().pattern(/^[a-z0-9-]+\.ethvault\.xyz$/);
+export const EnsNameValidator = jointz.string().pattern(/^[a-z0-9-]+\.myethvault\.com$/);
 
 export const CreateAccountParamsValidator = jointz.object().keys({
   name: NameValidator,
